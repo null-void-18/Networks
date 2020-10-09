c02938523a73f933f132f9878a2a4d2d96de7f82
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-10-09 19:55:23 +0530

    Update client.c

--- DNS Recursive using UDP/client.c
+++ DNS Recursive using UDP/client.c
@@ -13,18 +13,19 @@ int main(){
         int sockfd;
         struct sockaddr_in servaddr;
         char buff[1000];
-
+        //socket creation
         sockfd=socket(AF_INET,SOCK_DGRAM,0);
 
-
+        //defining the structure of the servaddr
         servaddr.sin_family=AF_INET;
         servaddr.sin_addr.s_addr=INADDR_ANY;
         servaddr.sin_port=htons(3000);
-
+        //port number 3000 is used to communicate between client and server
 
         bzero(buff,1000);
         fgets(buff,sizeof(buff),stdin);
-
+        
+        //send website name to server and receive address from server
         sendto(sockfd,(char *)buff,sizeof(buff),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
         bzero(buff,1000);
         recvfrom(sockfd,(char *)buff,1000,0,NULL,NULL);
