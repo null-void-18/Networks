1de21636aade42b6774ab3e3e66a954620bc1613
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-10-29 09:06:09 +0530

    Update clientchat.c

--- Chat application using UDP/clientchat.c
+++ Chat application using UDP/clientchat.c
@@ -1,3 +1,5 @@
+//This is the client code using UDP
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -13,21 +15,24 @@ int main(){
         struct sockaddr_in servaddr;
         char buff[1000];
 
+        //We use sock_dgram for udp
         sockfd=socket(AF_INET,SOCK_DGRAM,0);
 
-
+        //3000 is the port number for connection
         servaddr.sin_family=AF_INET;
         servaddr.sin_addr.s_addr=INADDR_ANY;
         servaddr.sin_port=htons(3000);
-
+        
+        //since the connection is udp , no three way handshake is needed for connection
+        
         while(1){
-        bzero(buff,1000);
-        fgets(buff,sizeof(buff),stdin);
-
-        sendto(sockfd,(char *)buff,sizeof(buff),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
-        bzero(buff,1000);
-        recvfrom(sockfd,(char *)buff,1000,0,NULL,NULL);
-        printf("Server : %s",buff);
+                bzero(buff,1000);
+                fgets(buff,sizeof(buff),stdin);
+                
+                sendto(sockfd,(char *)buff,sizeof(buff),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
+                bzero(buff,1000);
+                recvfrom(sockfd,(char *)buff,1000,0,NULL,NULL);
+                printf("Server : %s",buff);
         }
         return 0;
 }
