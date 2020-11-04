9b7e049ecc03275f80406d67f435a2a4e517dbda
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-11-04 10:51:36 +0530

    Update server.c

--- Nagle's Algorithm/server.c
+++ Nagle's Algorithm/server.c
@@ -17,6 +17,7 @@ int main(){
     char z[100];
     sockfd=socket(AF_INET,SOCK_STREAM,0);
 
+    //we use port number 4000 to communicate
     servaddr.sin_family=AF_INET;
     servaddr.sin_addr.s_addr=INADDR_ANY;
     servaddr.sin_port=htons(4000);
@@ -28,6 +29,8 @@ int main(){
     len=sizeof(cliaddr);
     clifd=accept(sockfd,(struct sockaddr*)&cliaddr,&len);
 
+    //Receive from client
+    //and send acknowledgement for the received packet
     while(1){
         recv(clifd,buff,100,0);
         if(strncmp(buff,"exit",4)==0){
